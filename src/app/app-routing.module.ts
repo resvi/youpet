d799fc3b039ffad95d049bcf54160738f48f3a2b
Reynaldo Esparragoza Villalba <53186564+resvi@users.noreply.github.com>
2022-01-22 20:52:32 -0500

    codigo innecesario que decidi borrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SectionServicesComponent } from './section-services/section-services.component';
 import { SetionServiceComponent } from './setion-service/setion-service.component';
-import { SectionLoverAnimalsComponent } from './section-lover-animals/section-lover-animals.component';
-import { SetionSalePetsComponent } from './setion-sale-pets/setion-sale-pets.component';
-import { SetionVetcareTeamComponent } from './setion-vetcare-team/setion-vetcare-team.component';
-import { SetionResultsComponent } from './setion-results/setion-results.component';
-import { SetionRecentPostComponent } from './setion-recent-post/setion-recent-post.component';
 
 const routes: Routes = [
   {
